8a53414a70a94039a6a2ae475b3649f1c80fbc53
Juan Sóto <juansheba97@gmail.com>
2023-02-09 09:07:05 +0000

    Deploy to test

--- content/main.js
+++ content/main.js
@@ -30,6 +30,8 @@ function showSetPinModal() {
 function handlePinInput() {
   const lcdText = document.querySelector(".lcd-text");
   const buttons = document.querySelectorAll(".button-pad");
+  const lcdScreen = document.querySelector(".lcd-screen")
+  const eyeIcon = document.querySelector(".eye-icon")
 
   buttons.forEach(function(button) {
     button.addEventListener("click", function() {
@@ -43,29 +45,42 @@ function handlePinInput() {
           lcdText.innerHTML = "SAVED";
           setTimeout(function() {
             lcdText.innerHTML = "";
+            showEnterPinModal();
           }, 2000);
           button.innerHTML = "ENTER";
           button.classList.remove("button-save");
           button.classList.add("button-enter");
         } else {
+          lcdScreen.style.backgroundColor = "#FFE4E4";
+          eyeIcon.style.display = "none";
           lcdText.innerHTML = "NOT A NUMBER";
           lcdText.classList.add("not-number")
           setTimeout(function() {
             lcdText.innerHTML = "";
+            eyeIcon.style.display = ""
             lcdText.classList.remove("not-number")
           }, 2000);
         }
       } else if (button.classList.contains("button-enter")) {
         if (lcdText.innerHTML === pin) {
-          window.location.href = "https://www.codebay-innovation.com/";
+          lcdScreen.style.backgroundColor = "#DEF4DA";
+          lcdText.innerHTML = "Correct";
+          eyeIcon.style.display = "none";
+          setTimeout(function() {
+            window.location.href = "https://www.codebay-innovation.com/";
+          }, 1000);
+          
         } else {
-          lcdText.classList.add("wrong")
+          lcdScreen.style.backgroundColor = "#FFE4E4";
+          eyeIcon.style.display = "none"
           lcdText.innerHTML = "WRONG";
           setTimeout(function() {
             lcdText.innerHTML = "";
-            lcdText.classList.remove("wrong")
+            eyeIcon.style.display = ""
+            lcdScreen.style.backgroundColor = "#E5F3F2"
           }, 2000);
           attempts--;
+          showErrorModal()
           if (attempts === 0) {
             window.location.href = "https://policia.es/";
           }
@@ -82,30 +97,60 @@ function showEnterPinModal() {
   const modal = document.createElement("div");
   modal.className = "modal";
   modal.innerHTML = `
-    <div class="modal-content">
-      <p>Please enter the previously set pin. You have ${attempts} attempts remaining.</p>
+    <div class="modal-content-2">
+    <img src="/assets/close.png" alt="" class="close-modal-2">
+      <p class="bienvenido-2">Introduce tu codigo</p>
+      <p class="instrucciones-2">Introduce el codigo de 6 digitos para continuar. (tienes ${attempts} intentos)</p>
     </div>
   `;
   document.body.appendChild(modal);
+
+  document.querySelector(".wall").style.webkitFilter = "blur(5px)"
+  const closeModal = document.querySelector(".close-modal-2");
+  closeModal.addEventListener("click", function() {
+    modal.style.display = "none";
+    document.querySelector(".wall").style.webkitFilter = "blur(0)"
+  });
+
+  
+    
 
   const lcdText = document.querySelector(".lcd-text");
   lcdText.addEventListener("input", function() {
     if (lcdText.innerHTML === pin) {
       window.location.href = "https://www.codebay-innovation.com/";
-    } else if (--attempts === 0) {
+    } else if (attempts === 0) {
       window.location.href = "https://policia.es/";
     } else {
+      --attempts;
+      showErrorModal()
       lcdText.innerHTML = "ERROR";
-      modal.querySelector("p").innerHTML = `Por favor, introduzca el código previamente establecido. Tiene ${attempts} intentos restantes.`;
     }
   });
-  }
+}
+
+function showErrorModal()  {
+  console.log("hola")
+  const modalError = document.createElement("div");
+  modalError.className = "modal-container";
+  modalError.innerHTML = `
+  <div class="modal-error">
+  <p class="text-error">Por favor, introduzca el código previamente establecido. Tiene ${attempts} intentos restantes. </p>
+  </div>
+  `;
+  // setTimeout(() => {
+  //   modalError.innerHTML = "";
+  // }, 2000);
   
+  document.body.appendChild(modalError);
+}
+
   // Function to initialize the app
   function init() {
   showSetPinModal();
   handlePinInput();
   }
+  
   
   // Initialize the app on window load
   window.addEventListener("load", init);
